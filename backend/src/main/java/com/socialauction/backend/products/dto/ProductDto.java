1bf4b998ae0deb71de19a10b4c0dda800feb1a73
yoonsung157 <yoonsung157@gmail.com>
2026-09-26 16:10:00 +0900

    _feat ProductDto 수정시간 추가

--- backend/src/main/java/com/socialauction/backend/products/dto/ProductDto.java
+++ backend/src/main/java/com/socialauction/backend/products/dto/ProductDto.java
@@ -11,14 +11,15 @@ import lombok.NoArgsConstructor;
 
 @AllArgsConstructor @Data @NoArgsConstructor @Builder 
 public class ProductDto {
-    private Integer productId;
-    private String name;
-    private Integer OrganizationId;
-    private Integer categoryId;
-    private Integer startPrice;
-    private String description;
-    private String background;
-    private LocalDateTime createdAt;
+    private Integer productId;      // 상품번호
+    private String name;            // 상품이름
+    private Integer OrganizationId; // 기관아이디
+    private Integer categoryId;     // 카테고리 아이디
+    private Integer startPrice;     // 시작가
+    private String description;     // 설명
+    private String background;      // 배경
+    private LocalDateTime createdAt;// 생성 시간
+    private LocalDateTime updatedAt;// 수정 시간
 
     public ProductEntity toEntity(){
         return ProductEntity.builder()
